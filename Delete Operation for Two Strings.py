8d5c36ffff21c2eb9fa10f493d5487d29e39137e
hecter_wu <hecter_wu@transcend.com.tw>
2017-06-09 16:55:49 +0800

    refactor: define Matrix more clearly

--- Delete Operation for Two Strings.py
+++ Delete Operation for Two Strings.py
@@ -22,11 +22,11 @@ class Solution():
     def minDistance(self, word1, word2):
         w = len(word1)
         h = len(word2)
-        Matrix = [[0 for x in range(w+1)] for y in range(h+1)] 
-        for x in range(h):
-            for y in range(w):
-                Matrix[x+1][y+1] = max(Matrix[x][y+1],Matrix[x+1][y],Matrix[x][y] + (word2[x] == word1[y]))
+        Matrix = [[0] * (h+1) for i in range(w+1)] 
+        for x in range(w):
+            for y in range(h):
+                Matrix[x+1][y+1] = max(Matrix[x][y+1],Matrix[x+1][y],Matrix[x][y] + (word1[x] == word2[y]))
         print(np.matrix(Matrix))
-        return w+h-(Matrix[h][w]*2)
+        return w+h-(Matrix[w][h]*2)
 if __name__ == '__main__':
     unittest.main()
