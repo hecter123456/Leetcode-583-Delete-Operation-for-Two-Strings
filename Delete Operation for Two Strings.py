8c14f16350c05d572c8f742208b20151239ca761
hecter_wu <hecter_wu@transcend.com.tw>
2017-06-09 16:28:58 +0800

    pass testcase: testDifferentLenWord by Correct Matrix index

--- Delete Operation for Two Strings.py
+++ Delete Operation for Two Strings.py
@@ -10,6 +10,10 @@ class unitest(unittest.TestCase):
         word1 = "sea"
         word2 = "eat"
         self.assertEqual(Solution().minDistance(word1,word2),2);
+    def testDifferentLenWord(self):
+        word1 = "a"
+        word2 = "ab"
+        self.assertEqual(Solution().minDistance(word1,word2),1);
 
 class Solution():
     def minDistance(self, word1, word2):
@@ -20,15 +24,15 @@ class Solution():
         exW = w+1
         exH = h+1
         Matrix = [[0 for x in range(exW)] for y in range(exH)] 
-        for x in range(exW):
-            for y in range(exH):
+        for x in range(exH):
+            for y in range(exW):
                 if not x or not y:
                     Matrix[x][y] = 0
-                elif word1[x-1] == word2[y-1]:
+                elif word2[x-1] == word1[y-1]:
                     Matrix[x][y] = Matrix[x-1][y-1]+1
                 else:
                     Matrix[x][y] = max(Matrix[x-1][y],Matrix[x][y-1])
         print(np.matrix(Matrix))
-        return w+h-(Matrix[w][h]*2)
+        return w+h-(Matrix[h][w]*2)
 if __name__ == '__main__':
     unittest.main()
